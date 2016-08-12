b748a994868a01e5ef544886cf53949f75b090d6
Voigt <mvoigt@eur.ad.sag>
2016-08-12 13:33:33 +0200

    initial commit

---
+++ src/main/java/com/voigt/hwd/client/navigation/NavigationData.java
@@ -0,0 +1,47 @@
+package com.voigt.hwd.client.navigation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.voigt.hwd.client.grid.history.HistoryAllTimeTable;
+import com.voigt.hwd.client.grid.history.HistoryOverview;
+import com.voigt.hwd.client.grid.history.HistoryStandingsChart;
+
+public class NavigationData {
+
+	public static final String ROOT = "root";
+
+	private static final Map<String, ExplorerTreeNode[]> navigationDataMap = new HashMap<String, ExplorerTreeNode[]>();
+
+	private final String idSuffix;
+
+	public NavigationData(String idSuffix) {
+		this.idSuffix = idSuffix;
+	}
+
+	private ExplorerTreeNode[] getData() {
+
+		ExplorerTreeNode[] data = navigationDataMap.get(idSuffix);
+
+		if (data == null) {
+			data = new ExplorerTreeNode[] {
+
+					new ExplorerTreeNode("HWD History", "history", ROOT, "silk/house.png", null, true, idSuffix),
+					new ExplorerTreeNode("Jahres&uuml;berblick", "history-overview", "history",
+							"silk/calendar_view_day.png", new HistoryOverview.Factory(), true, idSuffix),
+					new ExplorerTreeNode("Ewige Tabelle", "history-alltime-table", "history", "silk/calendar_add.png",
+							new HistoryAllTimeTable.Factory(), true, idSuffix),
+					new ExplorerTreeNode("9-Jahres-Platzierungen", "history-standings-chart", "history",
+							"silk/calendar_add.png", new HistoryStandingsChart.Factory(), true, idSuffix),
+
+			};
+			navigationDataMap.put(idSuffix, data);
+
+		}
+		return data;
+	}
+
+	public static ExplorerTreeNode[] getData(String idSuffix) {
+		return new NavigationData(idSuffix).getData();
+	}
+}
