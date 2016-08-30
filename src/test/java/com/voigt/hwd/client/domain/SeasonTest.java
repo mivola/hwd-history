ada7fc25285dbb6f00def2a744bcb3f4bbba1149
Michael Voigt <mivola@gmx.de>
2016-08-30 10:54:24 +0200

    add test for sorting of seasons

--- src/test/java/com/voigt/hwd/client/domain/SeasonTest.java
+++ src/test/java/com/voigt/hwd/client/domain/SeasonTest.java
@@ -1,6 +1,7 @@
 package com.voigt.hwd.client.domain;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,17 +25,27 @@ public class SeasonTest {
 
 	@Test
 	public void testAddingSeveralSeasons() throws Exception {
+		int startYear = 1979;
+		int endYear = 2979;
 
 		HistoryData.addSeason(new Season(1999));
 		HistoryData.addSeason(new Season(2000));
-		HistoryData.addSeason(new Season(2979));
+		HistoryData.addSeason(new Season(endYear));
 		HistoryData.addSeason(new Season(2000));
-		HistoryData.addSeason(new Season(1979));
-		HistoryData.addSeason(new Season(2979));
+		HistoryData.addSeason(new Season(startYear));
+		HistoryData.addSeason(new Season(endYear));
 
 		assertEquals(4, HistoryData.getSeasons().size());
-		assertEquals(2979, HistoryData.getEndYear());
-		assertEquals(1979, HistoryData.getStartYear());
+		assertEquals(endYear, HistoryData.getEndYear());
+		assertEquals(startYear, HistoryData.getStartYear());
+
+		// check sorting
+		int previousYear = 0;
+		for (Season season : HistoryData.getSeasons()) {
+			assertTrue(season.getYear() > previousYear);
+			previousYear = season.getYear();
+		}
+		assertEquals(endYear, previousYear);
 	}
 	
 }
