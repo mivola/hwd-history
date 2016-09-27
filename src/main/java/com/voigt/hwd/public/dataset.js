4b4bd65214b63a8ea45f40cf9afac923ab59b994
Michael Voigt <mivola@gmx.de>
2016-09-27 06:58:16 +0200

    remove obsolete stuff from SmartGWT

--- src/main/java/com/voigt/hwd/public/dataset.js
+++
@@ -1,7 +0,0 @@
-dataset = { 
-	Id: "unique id for this dataset", 
-	domain: [ UTCTimeInMilliseconds_1, UTCtimeInMilliseconds_2 ], 
-	range: [ Value_1, Value_2, ... ], 
-	label: "default label for this dataset", 
-	axis: "unit" 
-} 
